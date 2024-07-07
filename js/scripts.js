2beab26172588809089233f6e7f8dfc2254f12e7
Sivagami <112805637+SivaHub125@users.noreply.github.com>
2024-07-07 11:08:07 +0530

    Portfolio

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+/* =======  typing animation ============= */
+var typed = new Typed(".typing",{
+    strings:["","Computer Science Student","Web Developer" ,"Coder","Learner"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+
+})
